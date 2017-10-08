5b8302fc9703d229b42798a96231ceb6d5dc7d87
martinci <relotic@gmail.com>
2017-10-09 00:17:44 +0200

    Passed first 2 problems in week 5

--- 1 - Algorithmic Toolbox/week5 - dynamic programming/assignment/knapsack.cpp
+++ 1 - Algorithmic Toolbox/week5 - dynamic programming/assignment/knapsack.cpp
@@ -3,23 +3,62 @@
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+// Greedy algorithm, suboptimal solution
+int greedy_weight(int W, const vector<int> &weights) {
+    int current_weight = 0;
+    for (size_t i = 0; i < weights.size(); ++i) {
+        if (current_weight + weights[i] <= W) {
+            current_weight += weights[i];
+        }
     }
-  }
-  return current_weight;
+    return current_weight;
+}
+
+// Helper function to output a matrix in the console.
+void display(vector<vector<int>> &matrix) {
+    for (auto &row : matrix) {
+        for (auto &elem : row) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int optimal_weight(int W, const vector<int> &weights) {
+    // Dynamic programming approach, set up a (n+1)x(W+1) matrix of results.
+    // Note that it is initialized to 0s by default, which works for us.
+    int n = weights.size();
+    vector<vector<int>> optimal_results(n + 1);
+    for (auto &row : optimal_results) {
+        row.resize(W + 1);
+    }
+
+    for (int K = 1; K <= W; ++K) {
+        for (int i = 1; i <= n; ++i) {
+            // Compute solution for total weight K only using up to the i-th weight.
+            int max_weight = optimal_results[i - 1][K];
+            // Need to shift i to match the weight index
+            int w = weights[i - 1];
+            if (w <= K) {
+                int temp_max = optimal_results[i - 1][K - w] + w;
+                if (temp_max > max_weight)
+                    max_weight = temp_max;
+            }
+            optimal_results[i][K] = max_weight;
+        }
+    }
+    // display(optimal_results);
+    return optimal_results[n][W];
 }
 
 int main() {
-  int n, W;
-  std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
-  std::cout << optimal_weight(W, w) << '\n';
+    int n, W;
+    std::cin >> W >> n;
+    vector<int> weights(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> weights[i];
+    }
+    std::cout << optimal_weight(W, weights) << '\n';
+    // std::cin >> n;
 }
--- 1 - Algorithmic Toolbox/week5 - dynamic programming/assignment/primitive_calculator.cpp
+++ 1 - Algorithmic Toolbox/week5 - dynamic programming/assignment/primitive_calculator.cpp
@@ -1,88 +1,112 @@
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 using std::vector;
+using std::unordered_map;
 
-vector<int> optimal_sequence(int n) {
-    std::vector<int> sequence;
-    while (n >= 1) {
-        sequence.push_back(n);
-        if (n % 3 == 0) {
-            n /= 3;
-        } else if (n % 2 == 0) {
-            n /= 2;
-        } else {
-            n = n - 1;
+vector<int> minimum_operations(int n) {
+    // Since the minimum operations of n depends on the previous ones,
+    // we compute min operations from 1 up to n
+    vector<int> minimums(n + 1);
+    minimums[1] = 0; // Redundant, included for clarity
+
+    for (int i = 2; i <= n; ++i) {
+        // Take the samallest previous min
+        int min = minimums[i - 1] + 1;
+        if (i % 2 == 0) {
+            int temp_min = minimums[i / 2] + 1;
+            if (temp_min < min) {
+                min = temp_min;
+            }
         }
+        if (i % 3 == 0) {
+            int temp_min = minimums[i / 3] + 1;
+            if (temp_min < min) {
+                min = temp_min;
+            }
+        }
+        minimums[i] = min;
     }
+
+    return minimums;
+}
+
+vector<int> recover_sequence(const int n, const vector<int> &minimums) {
+    vector<int> sequence;
+    int idx{n};
+    do {
+        sequence.push_back(idx);
+        int min_idx = idx - 1;
+        int min = minimums[min_idx];
+        if (idx % 2 == 0) {
+            int temp_min = minimums[idx / 2];
+            if (temp_min < min) {
+                min = temp_min;
+                min_idx = idx / 2;
+            }
+        }
+        if (idx % 3 == 0) {
+            int temp_min = minimums[idx / 3];
+            if (temp_min < min) {
+                min = temp_min;
+                min_idx = idx / 3;
+            }
+        }
+        idx = min_idx;
+    } while (idx >= 1);
+
     reverse(sequence.begin(), sequence.end());
     return sequence;
 }
 
-int min_operations(int n, vector<int> &memory, vector<int> &sequence) {
-    if (n < 1) {
-        return -1;
-    }
-    if (n == 1) {
-        return 0;
-    }
-    if (memory[n] != 0) {
-        return memory[n];
-    }
-    // vector to hold the cases for n/2, n/3, n-1, if applicable, in this order
-    vector<int> prev_mins(3);
-    for (auto &min : prev_mins) {
-        min = -1;
-    }
-    if (n % 2 == 0) {
-        prev_mins[0] = min_operations((int)n / 2, memory, sequence);
-    }
-    if (n % 3 == 0) {
-        prev_mins[1] = min_operations((int)n / 3, memory, sequence);
-    }
-    prev_mins[2] = min_operations(n - 1, memory, sequence);
-
-    // Find minimum and index to keep track of results
-    size_t min_idx = 0;
-    int min = -1;
-    for (size_t i = 0; i < prev_mins.size(); ++i) {
-        int temp_min = prev_mins[i];
-        if (temp_min != -1) {
-            if (min == -1) {
-                min = temp_min;
-                min_idx = i;
-            } else if (temp_min < min) {
-                min = temp_min;
-                min_idx = i;
-            }
-        }
-    }
-    // Add element to sequence
-    if (min_idx == 0) {
-        sequence.push_back((int)n / 2);
-    } else if (min_idx == 1) {
-        sequence.push_back((int)n / 3);
-    } else {
-        sequence.push_back(n - 1);
-    }
-    for (auto num : sequence) {
+vector<int> optimal_sequence(int n) {
+    std::vector<int> minimums = minimum_operations(n);
+    /*for (auto num : minimums) {
         std::cout << num << " ";
     }
-    std::cout << std::endl;
-    return min + 1;
+    std::cout << std::endl;*/
+    std::vector<int> sequence = recover_sequence(n, minimums);
+    return sequence;
+}
+
+int min_operations_memoization(int n, unordered_map<int, int> &memory) {
+    if (memory.count(n) == 1) {
+        // std::cout << "Found one " << n << std::endl;
+        return memory.at(n);
+    }
+    if (n == 1) {
+        // The function only gets here the first time it processes 1.
+        memory[1] = 0;
+        return 0;
+    }
+
+    int min = min_operations_memoization(n - 1, memory) + 1;
+    if (n % 2 == 0) {
+        int temp_min = min_operations_memoization(n / 2, memory) + 1;
+        if (min == -1 or temp_min < min) {
+            min = temp_min;
+        }
+    }
+    if (n % 3 == 0) {
+        int temp_min = min_operations_memoization(n / 3, memory) + 1;
+        if (min == -1 or temp_min < min) {
+            min = temp_min;
+        }
+    }
+
+    memory[n] = min;
+    return min;
 }
 
 int main() {
     int n;
     std::cin >> n;
-    vector<int> memory(n + 1);
-    vector<int> sequence;
-    int min_ops = min_operations(n, memory, sequence);
-    std::cout << min_ops << std::endl;
+    vector<int> sequence = optimal_sequence(n);
+    std::cout << sequence.size() - 1 << std::endl;
     for (auto num : sequence) {
         std::cout << num << " ";
     }
-    std::cout << n;
-    std::cin >> n;
+    // std::cin >> n;
 }
