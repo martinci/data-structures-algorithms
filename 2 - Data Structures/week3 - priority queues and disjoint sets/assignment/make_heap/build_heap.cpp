c036c9bc30f36bcb73aef48a5058a57e42afa98b
martinci <relotic@gmail.com>
2017-10-30 00:30:39 +0100

    Finished problem 1 in week3 of 2nd course.

--- 2 - Data Structures/week3 - priority queues and disjoint sets/assignment/make_heap/build_heap.cpp
+++ 2 - Data Structures/week3 - priority queues and disjoint sets/assignment/make_heap/build_heap.cpp
@@ -11,40 +11,53 @@ using std::make_pair;
 
 class HeapBuilder {
   private:
-    vector<int> data_;
-    vector<pair<int, int>> swaps_;
+    vector<int> _data;
+    vector<pair<int, int>> _swaps;
 
     void WriteResponse() const {
-        cout << swaps_.size() << "\n";
-        for (int i = 0; i < swaps_.size(); ++i) {
-            cout << swaps_[i].first << " " << swaps_[i].second << "\n";
+        cout << _swaps.size() << "\n";
+        for (int i = 0; i < _swaps.size(); ++i) {
+            cout << _swaps[i].first << " " << _swaps[i].second << "\n";
         }
     }
 
     void ReadData() {
         int n;
         cin >> n;
-        data_.resize(n);
+        _data.resize(n);
         for (int i = 0; i < n; ++i)
-            cin >> data_[i];
+            cin >> _data[i];
+    }
+
+    int GetMinChild(int idx) {
+        int left = 2 * idx + 1;
+        int right = 2 * idx + 2;
+        if (left >= _data.size()) {
+            return idx;
+        } else if (right >= _data.size()) {
+            return left;
+        } else {
+            return _data[left] < _data[right] ? left : right;
+        }
+    }
+
+    void SiftDown(int idx) {
+        int val = _data[idx];
+        int min_child = GetMinChild(idx);
+        while (_data[min_child] < val) {
+            _swaps.push_back(make_pair(idx, min_child));
+            _data[idx] = _data[min_child];
+            _data[min_child] = val;
+            idx = min_child;
+            min_child = GetMinChild(idx);
+        }
     }
 
     void GenerateSwaps() {
-        swaps_.clear();
-        // The following naive implementation just sorts
-        // the given sequence using selection sort algorithm
-        // and saves the resulting sequence of swaps.
-        // This turns the given array into a heap,
-        // but in the worst case gives a quadratic number of swaps.
-        //
-        // TODO: replace by a more efficient implementation
-        for (int i = 0; i < data_.size(); ++i)
-            for (int j = i + 1; j < data_.size(); ++j) {
-                if (data_[i] > data_[j]) {
-                    swap(data_[i], data_[j]);
-                    swaps_.push_back(make_pair(i, j));
-                }
-            }
+        _swaps.clear();
+        for (int i = (int)_data.size() / 2; i >= 0; i--) {
+            SiftDown(i);
+        }
     }
 
   public:
@@ -59,5 +72,7 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     HeapBuilder heap_builder;
     heap_builder.Solve();
+    // int n;
+    // cin >> n;
     return 0;
 }
