41abaabeadb8c171522c879909a39b0a4fa1830a
martinci <relotic@gmail.com>
2017-11-01 00:23:11 +0100

    Finished 3rd problem in week3 of 2nd course.

--- 2 - Data Structures/week3 - priority queues and disjoint sets/assignment/merging_tables/merging_tables.cpp
+++ 2 - Data Structures/week3 - priority queues and disjoint sets/assignment/merging_tables/merging_tables.cpp
@@ -11,10 +11,9 @@ using std::max;
 using std::vector;
 
 struct DisjointSetsElement {
-    int size, parent, rank;
+    int size, parent;
 
-    DisjointSetsElement(int size = 0, int parent = -1, int rank = 0)
-        : size(size), parent(parent), rank(rank) {}
+    DisjointSetsElement(int size = 0, int parent = -1) : size(size), parent(parent) {}
 };
 
 struct DisjointSets {
@@ -29,15 +28,26 @@ struct DisjointSets {
 
     int getParent(int table) {
         // find parent and compress path
+        int idx{table};
+        if (sets[idx].parent != idx) {
+            sets[idx].parent = getParent(sets[idx].parent);
+        }
+        return sets[idx].parent;
     }
 
     void merge(int destination, int source) {
         int realDestination = getParent(destination);
         int realSource = getParent(source);
+        // std::cout << realDestination << " " << realSource << std::endl;
         if (realDestination != realSource) {
+            DisjointSetsElement &source = sets[realSource];
+            DisjointSetsElement &destination = sets[realDestination];
             // merge two components
-            // use union by rank heuristic
+            source.parent = realDestination;
+            destination.size += source.size;
+            source.size = 0;
             // update max_table_size
+            max_table_size = max(max_table_size, destination.size);
         }
     }
 };
